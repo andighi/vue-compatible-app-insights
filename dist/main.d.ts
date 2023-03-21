d99a1b5ed4876dc0d00c7afe0a6353d3e776f3d4
realandicrist <128487873+realandicrist@users.noreply.github.com>
2023-03-21 15:32:19 +0200

    Add dist and package.lock

---
+++ dist/main.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Install function passed to Vue.use() or app.use() show documentation on vue.js website.
+ *
+ * @param app
+ * @param options
+ */
+declare function install(app: any, options: any): void;
+export default install;
